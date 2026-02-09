4fb441539565ffcc73c44b78c5bfb8d904779df4
jesparr <jsprsvson@gmail.com>
2026-02-09 11:04:43 +0100

    tog bort fil vvv

--- Veckouppgift4/vvv.py
+++
@@ -1,9 +0,0 @@
-def average_words(strings):
-    found = []
-    for item in strings:
-        if 4 < len(item) < 8:
-            found.append(item)
-    return found
-
-average_words(["sup", "how's", "it", "going", "reflecting", "on", "programs", "and", "coding"])
-print(average_words)
